8c2cbc72fd24a0aa9fb1677ad416f241df06b66f
Dana Criollo <dana.criollo.ljfj@gmail.com>
2023-08-13 23:06:24 -0500

    vista vincular-proyecto-coordinador con estilos y filtro buscador - FE

--- resources/views/Coordinador/vista_vincular_proyecto.blade.php
+++ resources/views/Coordinador/vista_vincular_proyecto.blade.php
@@ -3,73 +3,123 @@
 @section('title', 'Participantes')
 
 @section('content_header')
-<h1>Listado de Proyectos </h1>
+
+<div class="container">
+    <div class="note note-success mb-3">
+        <figure class="text-center">
+            <h1>Listado de Proyectos </h1>
+        </figure>
+    </div>
+</div>
+
+
 @stop
 
 @section('content')
-    <p>Bienvenido {{ $user->name }}</p>
-    <br>
-    <center>
-        <table id="buscador-agregar">
-            <tr>
-                <td>
-                    <div id="contenedor-buscador" class="input-group">
-                        <div id="inp">
-                            <input id ="buscador" type="text" placeholder="Buscar proyecto">
-                        </div>
-                        <div id="ic">
-                            <i class="fas fa-search"></i>
-                        </div>
-                    </div>
-                </td>
-            </tr>
-        </table>    
-    </center>
-    <br>
-    <table id= "tabla_usuarios" class="table">
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Id</th>
-                <th scope="col">Nombre del Proyecto</th>
-                <th scope="col">Estado</th>
-                <th scope="col">Tipo de Proyecto</th>
-                <th scope="col">Fecha inicio</th>
-                <th scope="col">Fecha Finalización</th>
-                <th scope="col">Propuesta</th>
-                <th scope="col">Proyecto Final</th>
-                <th scope="col">Opciones</th>
-            </tr>
-        </thead>
-        <tbody>
-            @php
-                $i=1;
-            @endphp
-            @foreach($proyectos as $p)
-                <tr>
-                    <th scope="row">{{$i}}</th>
-                    <td>{{$p->id_proyecto}}</td>
-                    <td>{{$p->titulo}}</td>
-                    <td>{{$p->estado}}</td>
-                    <td>{{$p->tipo_proyecto}}</td>
-                    <td>{{$p->feacha_inicio}}</td>
-                    <td>{{$p->feacha_fin}}</td>
-                    <td><a href="{{ asset($p->arc_propuesta) }}" target="_blank">Descargar PDF</a></td>
-                    <td><a href="{{ asset($p->arc_adjunto) }}" target="_blank">Descargar PDF</a></td>
-                    <td>
-                        <a href="{{route('vincular_sem_proyecto', ['id_proyecto' => $p->id_proyecto, 'num_identificacion' => $num_identificacion])}}" class="btn btn-primary">Vincular</a>
-                        @if($p->id !== $user->id)
-                        <a href="{{route('desvincular_sem_proy', ['id_proyecto' => $p->id_proyecto, 'num_identificacion' => $num_identificacion])}}" class="btn btn-danger">Desvincular</>
-                        @endif
-                    </td>
-                </tr>
-                @php
-                    $i++;
-                @endphp
-            @endforeach
-        </tbody>
-    </table>
+<div class="container">
 
+    <center>
+
+    <br>
+    <ul class="list-unstyled">
+        <li class="mb-1"><i class="fas fa-check-circle me-2 text-success"></i>Bienvenido {{ $user->name }}</li>
+    </ul>  
+    <br>
+
+    <br>
+    <table id="buscador-agregar">
+        <tr>
+            <td>
+                <div id="contenedor-buscador" class="input-group">
+                    <div id="inp">
+                        <input id ="buscador" type="text" placeholder="Buscar proyecto">
+                    </div>
+                    <div id="ic">
+                        <i class="fas fa-search"></i>
+                    </div>
+                </div>
+            </td>
+        </tr>
+    </table>   
+
+    </center>
+
+    <br>
+    <div class="tabla-container" style= "overflow-x: auto;">
+
+        <table id= "tabla_usuarios" class="table">
+            <thead class="table-info">
+                <tr>
+                    <th scope="col"> </th>
+                    <th scope="col">Id</th>
+                    <th scope="col">Nombre del Proyecto</th>
+                    <th scope="col">Estado</th>
+                    <th scope="col">Tipo de Proyecto</th>
+                    <th scope="col">Fecha inicio</th>
+                    <th scope="col">Fecha Finalización</th>
+                    <th scope="col">Propuesta</th>
+                    <th scope="col">Proyecto Final</th>
+                    <th scope="col">Opciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                @php
+                    $i=1;
+                @endphp
+                @foreach($proyectos as $p)
+                    <tr>
+                        <th scope="row">{{$i}}</th>
+                        <td>{{$p->id_proyecto}}</td>
+                        <td>{{$p->titulo}}</td>
+                        <td>{{$p->estado}}</td>
+                        <td>{{$p->tipo_proyecto}}</td>
+                        <td>{{$p->feacha_inicio}}</td>
+                        <td>{{$p->feacha_fin}}</td>
+                        <td><a href="{{ asset($p->arc_propuesta) }}" target="_blank">Descargar PDF</a></td>
+                        <td><a href="{{ asset($p->arc_adjunto) }}" target="_blank">Descargar PDF</a></td>
+                        <td>
+                            <center>
+                            <a style="margin: 3px;" href="{{route('vincular_sem_proyecto', ['id_proyecto' => $p->id_proyecto, 'num_identificacion' => $num_identificacion])}}" class="btn btn-primary btn-sm">Vincular</a>
+                            @if($p->id !== $user->id)
+                            <a style="margin: 3px;" href="{{route('desvincular_sem_proy', ['id_proyecto' => $p->id_proyecto, 'num_identificacion' => $num_identificacion])}}" class="btn btn-danger btn-sm">Desvincular</a>
+                            @endif
+                            </center>
+                        </td>
+                    </tr>
+                    @php
+                        $i++;
+                    @endphp
+                @endforeach
+            </tbody>
+        </table>
+
+    </div>
+
+
+    <script>
+
+        //buscador
+        document.addEventListener("DOMContentLoaded", function() {
+            var filtroBusqueda = document.getElementById("buscador");
+
+            filtroBusqueda.addEventListener("keyup", function() {
+                var valorBusqueda = filtroBusqueda.value.toLowerCase();
+                var filas = document.querySelectorAll("#tabla_usuarios tbody tr");
+
+                filas.forEach(function(fila) {
+                    var contenidoFila = fila.textContent.toLowerCase();
+                    if (contenidoFila.indexOf(valorBusqueda) !== -1) {
+                        fila.style.display = "table-row";
+                    } else {
+                        fila.style.display = "none";
+                    }
+                });
+            });
+        });
+
+    </script>
+
+</div>
     @if (session('vinculacionExitosa'))
         <script>
             document.addEventListener('DOMContentLoaded', function() {
