10fdb861b50925caf4894429cf32e08315a3f999
Dana Criollo <dana.criollo.ljfj@gmail.com>
2023-08-24 16:45:07 -0500

    vista editar-eventos con estilos - FE

--- resources/views/Admin/vista_edit_eventos.blade.php
+++ resources/views/Admin/vista_edit_eventos.blade.php
@@ -3,168 +3,175 @@
 @section('title', 'Editar Evento')
 
 @section('content_header')
-    <h1>Editar Evento</h1>
+
+    <div class="container">
+        <div class="mb-3 note note-success">
+            <figure class="text-center">
+            <h1>Editar Evento</h1>
+            </figure>
+        </div>
+    </div>
+    
 @stop
 
 @section('content')
-    <p>Bienvenido {{ $user->name }}</p>
-    <br>
+<div class="container">
     <center>
-        <div id="contenedor-form">
-            <form method="POST" action="" action="{{ route('editar_evento','codigo_evento') }}">
-                @csrf
-                <div class="row">
-                    <div class="col">
-                        <div>
-                            @error('codigo_evento')
-                                <span class="text-danger">{{ $message }}</span>
-                            @enderror
-                            <div class="mb-4 form-outline">
-                                <input type="text" id="codigo_evento" name="codigo_evento" class="form-control" value="{{$evento_id->codigo_evento }}" />
-                                <label class="form-label" for="codigo_evento">Id del Evento</label>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div>
-                            @error('nombre')
-                                <span class="text-danger">{{ $message }}</span>
-                            @enderror
-                            <div class="mb-4 form-outline">
-                                <input type="text" id="nombre" name="nombre" class="form-control" value="{{$evento_id->nombre}}"/>
-                                <label class="form-label" for="nombre">Nombre del Evento</label>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div>
-                            @error('fecha_inicio')
-                                <span class="text-danger">{{ $message }}</span>
-                            @enderror
-                            <div class="form-outline">
-                                <input class="form-control" type="date" id="fecha_inicio" name="fecha_inicio" placeholder="Fecha de inicio" value="{{$evento_id->fecha_inicio}}">
-                                <label class="form-label" for="fecha_inicio">Fecha de Inicio</label>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div>
-                            @error('fecha_fin')
-                                <span class="text-danger">{{ $message }}</span>
-                            @enderror
-                            <div class="form-outline">
-                                <input class="form-control" type="date" id="fecha_fin" name="fecha_fin" placeholder="Fecha de fin" value="{{$evento_id->fecha_fin}}">
-                                <label class="form-label" for="fecha_fin">Fecha de Finalización</label>
-                            </div>
-                        </div>
-                    </div>
-                    
-                    
-                </div>
+    <br>
+    <ul class="list-unstyled">
+    <li class="mb-1"><i class="fas fa-check-circle me-2 text-success"></i>Bienvenido {{ $user->name }}</li>
+    </ul> 
+    <br>
 
-                <div class="row">
-                    <div class="col">
-                        <div>
-                            @error('correo')
-                                <span class="text-danger">{{ $message }}</span>
-                            @enderror
-                            <div class="mb-4 form-outline">
-                                <input type="text" id="correo" name="correo" class="form-control"  />
-                                <label class="form-label" for="correo">Correo</label>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div>
-                            @error('telefono')
-                                <span class="text-danger">{{ $message }}</span>
-                            @enderror
-                            <div class="mb-4 form-outline">
-                                <input type="text" id="telefono" name="telefono" class="form-control" />
-                                <label class="form-label" for="telefono">Numero de Contacto</label>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div>
-                            @error('lugar')
-                                <span class="text-danger">{{ $message }}</span>
-                            @enderror
-                            <div class="mb-4 form-outline">
-                                <input type="text" id="lugar" name="lugar" class="form-control" value="{{$evento_id->lugar}}"/>
-                                <label class="form-label" for="lugar">Lugar</label>
-                            </div>
-                        </div>
-                    </div>
-                    
+    <br>
+    <div id="contenedor-form">
+        <form class="row g-3 needs-validation" novalidate method="POST" action="" action="{{ route('editar_evento','codigo_evento') }}">
+            @csrf
+            <!-- Id del Evento -->
+            <div class="col-md-4">
+                <div class="form-outline">
+                    <input type="text" id="codigo_evento" name="codigo_evento" class="form-control is-valid" value="{{$evento_id->codigo_evento }}"/>
+                    <label class="form-label" for="codigo_evento">Id del Evento</label>
                 </div>
-                <div class="row">
-                    <div class="col">
-                        @error('tipo')
-                            <span class="text-danger">{{ $message }}</span>
-                        @enderror
-                        <select id="tipo" name="tipo" class="form-select">
-                            <option value="">Tipo</option>
-                            <option value="1" {{ $evento_id->tipo == '1' ? 'selected' : '' }}>Congreso</option>
-                            <option value="2" {{ $evento_id->tipo == '2' ? 'selected' : '' }}>Encuentro</option>
-                            <option value="3" {{ $evento_id->tipo == '3' ? 'selected' : '' }}>Seminario</option>
-                            <option value="4" {{ $evento_id->tipo == '4' ? 'selected' : '' }}>Taller</option>
-                        </select>
-                    </div>
-                    <div class="col">
-                        @error('modalidad')
-                            <span class="text-danger">{{ $message }}</span>
-                        @enderror
-                        <select id ="modalidad" name="modalidad" class="form-select">
-                            <option selected>Modalidad</option>
-                            <option value="1" {{ $evento_id->modalidad == '1' ? 'selected' : '' }}>Virtual</option>
-                            <option value="2" {{ $evento_id->modalidad == '2' ? 'selected' : '' }}>Presencial</option>
-                            <option value="3" {{ $evento_id->modalidad == '3' ? 'selected' : '' }}>Hibrida</option>
-                        </select>
-                    </div>
-                    <div class="col">
-                        @error('clasificacion')
-                            <span class="text-danger">{{ $message }}</span>
-                        @enderror
-                        <select id ="clasificacion" name="clasificacion" class="form-select">
-                            <option selected>Clasificación</option>
-                            <option value="1" {{ $evento_id->clasificacion == '1' ? 'selected' : '' }}>Local</option>
-                            <option value="2" {{ $evento_id->clasificacion == '2' ? 'selected' : '' }}>Regional</option>
-                            <option value="3" {{ $evento_id->clasificacion == '3' ? 'selected' : '' }}>Nacional</option>
-                        </select>
-                    </div>
+                @error('codigo_evento')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
+
+            <!-- Nombre del Evento -->
+            <div class="col-md-8">
+                <div class="form-outline">
+                <input type="text" id="nombre" name="nombre" class="form-control is-valid" value="{{$evento_id->nombre}}"/>
+                <label class="form-label" for="nombre">Nombre del Evento</label>
                 </div>
-                <br>
-                <div class="row">
-                    <div class="col">
-                        <div>
-                            @error('descripcion')
-                                <span class="text-danger">{{ $message }}</span>
-                            @enderror
-                            <div class="form-outline">
-                                <textarea class="form-control" id="descripcion" name="descripcion" rows="3" >{{ $evento_id->descripcion }}</textarea>
-                                <label class="form-label" for="descripcion">Descripción</label>
-                              </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div>
-                            @error('observaciones')
-                                <span class="text-danger">{{ $message }}</span>
-                            @enderror
-                            <div class="form-outline">
-                                <textarea class="form-control" id="observaciones" name="observaciones" rows="3">{{ $evento_id->observaciones }}</textarea>
-                                <label class="form-label" for="observaciones">Observaciones</label>
-                              </div>
-                        </div>
-                    </div>
+                @error('nombre')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
+
+            <!-- Fecha inicio -->
+            <div class="col-md-6">
+                <label class="form-label" for="fecha_inicio" id="lb">Fecha de Inicio</label>
+                <input class="form-control is-valid" type="date" id="fecha_inicio" name="fecha_inicio" placeholder="Fecha de inicio" value="{{$evento_id->fecha_inicio}}">
+                @error('fecha_inicio')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
+
+            <!-- Fecha de finalización -->
+            <div class="col-md-6">
+                <label class="form-label" for="fecha_fin" id="lb">Fecha de Finalización</label>
+                <input class="form-control is-valid" type="date" id="fecha_fin" name="fecha_fin" placeholder="Fecha de fin" value="{{$evento_id->fecha_fin}}">
+                @error('fecha_fin')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
+
+            <!-- Correo-->
+            <div class="col-md-4">
+                <div class="form-outline">
+                    <input type="email" id="correo" name="correo" class="form-control is-valid"/>
+                    <label class="form-label" for="correo">Correo</label>
                 </div>
-                <br>
-                <!-- Boton Enviar -->
-                <button type="submit" class="mb-3 btn btn-success btn-block">Actualizar Evento</button>
-            </form>
-        </div>
+                @error('correo')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
+
+            <!-- Telefono -->
+            <div class="col-md-4">
+                <div class="form-outline">
+                    <input type="text" id="telefono" name="telefono" class="form-control is-valid"/>
+                    <label class="form-label" for="telefono">Número de Contacto</label>
+                </div>
+                @error('telefono')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
+
+            <!-- Dirección -->
+            <div class="col-md-4">
+                <div class="form-outline">
+                    <input type="text" id="lugar" name="lugar" class="form-control is-valid" value="{{$evento_id->lugar}}"/>
+                    <label class="form-label" for="lugar">Lugar</label>
+                </div>
+                @error('lugar')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
+
+            <!-- tipo de evento -->
+            <div class="col-md-4">
+                <label class="form-label" for="tipo_proyecto" id="lb">Seleccione: </label>
+                <select id ="tipo" name="tipo" class="form-select is-valid">
+                    <option value="">Tipo</option>
+                    <option value="1" {{ $evento_id->tipo == '1' ? 'selected' : '' }}>Congreso</option>
+                    <option value="2" {{ $evento_id->tipo == '2' ? 'selected' : '' }}>Encuentro</option>
+                    <option value="3" {{ $evento_id->tipo == '3' ? 'selected' : '' }}>Seminario</option>
+                    <option value="4" {{ $evento_id->tipo == '4' ? 'selected' : '' }}>Taller</option>
+                </select>
+                @error('tipo')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
+
+            <!-- modalidad -->
+            <div class="col-md-4">
+                <label class="form-label" for="tipo_proyecto" id="lb">Seleccione: </label>
+                <select id ="modalidad" name="modalidad" class="form-select is-valid">
+                    <option selected>Modalidad</option>
+                    <option value="1" {{ $evento_id->modalidad == '1' ? 'selected' : '' }}>Virtual</option>
+                    <option value="2" {{ $evento_id->modalidad == '2' ? 'selected' : '' }}>Presencial</option>
+                    <option value="3" {{ $evento_id->modalidad == '3' ? 'selected' : '' }}>Hibrida</option>
+                </select>
+                @error('modalidad')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
+
+            <!-- clasificación -->
+            <div class="col-md-4">
+                <label class="form-label" for="tipo_proyecto" id="lb">Seleccione: </label>
+                <select id ="clasificacion" name="clasificacion" class="form-select is-valid">
+                    <option selected>Clasificación</option>
+                    <option value="1" {{ $evento_id->clasificacion == '1' ? 'selected' : '' }}>Local</option>
+                    <option value="2" {{ $evento_id->clasificacion == '2' ? 'selected' : '' }}>Regional</option>
+                    <option value="3" {{ $evento_id->clasificacion == '3' ? 'selected' : '' }}>Nacional</option>
+                </select>
+                @error('clasificacion')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
+
+            <!-- Descripción -->
+            <div class="col-md-6">
+                <div class="form-outline">
+                    <textarea class="form-control is-valid" id="descripcion" name="descripcion" rows="3">{{ $evento_id->descripcion }}</textarea>
+                    <label class="form-label" for="descripcion">Descripción</label>
+                </div>
+                @error('descripcion')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
+
+            <!-- Observaciones -->
+            <div class="col-md-6">
+                <div class="form-outline">
+                    <textarea class="form-control is-valid" id="observaciones" name="observaciones" rows="3">{{ $evento_id->observaciones }}</textarea>
+                    <label class="form-label" for="observaciones">Observaciones</label>
+                </div>
+                @error('observaciones')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
+
+            <!-- Boton Enviar -->
+            <button type="submit" class="mb-3 btn btn-success btn-block">Actualizar Evento</button>
+        </form>
+    </div>
+
     </center>
+</div>
     @if (session('registroExitoso'))
         <script>
             document.addEventListener('DOMContentLoaded', function() {
@@ -209,6 +216,7 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.0/mdb.min.css" rel="stylesheet"/>
     <!--CSS Propio-->
     <link href="{{ asset('css/segundo/general.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/segundo/perfil.css')}}">
 @endsection
 
 @section('js')
