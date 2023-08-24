aafd3fb102b8571f416bd27d817c5c715768ce12
Dana Criollo <dana.criollo.ljfj@gmail.com>
2023-08-24 17:51:45 -0500

    vista actualizar-info-semillerista con estilos - FE

--- resources/views/Semillerista/actualizar-info.blade.php
+++ resources/views/Semillerista/actualizar-info.blade.php
@@ -3,114 +3,116 @@
 @section('title', 'Perfil')
 
 @section('content_header')
-    <div class="container">
-        <div class="row">
-            <center>
-                <div class="col">
-                    @if(isset($persona) && $persona->foto !== null)
-                        <img class="foto-perfil" src="{{ Storage::url($persona->foto)}}" alt="Foto de Perfil">
-                    @endif
-                    <center>
-                        <div>
-                            <p>Bienvenido {{ $user->name }}, Su rol es Semillerista</p>
-                        </div>
-                    </center>
-                </div>
-            </center>
-        </div>
+<div class="container">
+    <div class="mb-3 note note-success">
+        <figure class="text-center">
+            <h1>Informacion Académica</h1>
+        </figure>
     </div>
+</div>
+
 @stop
 
 @section('content')
-    <div class="container">
-        <br>
-        <center>
-            <div class="col">
-                <h1>Informacion Académica</h1>
-            </div>
-        </center>
-        <p>Por favor asegurece de que sus datos Académicos Esten Actualizados</p>
-        <br>
-        <div id="contenedor-perfil">
-            <form action="{{ route('actualizar_datos_semillerista') }}" method="POST" enctype="multipart/form-data">
-            @csrf
-                <div class="row">
-                <div class="col">
-                    @error('cod_estudiante')
-                        <span class="text-danger">{{ $message }}</span>
-                    @enderror
-                    <div class="form-outline">
-                    <input type = "text" id="cod_estudiante" name="cod_estudiante" class="form-control" value="{{ isset($semillerista) ? $semillerista->cod_estudiante : old('cod_estudiante') }}"/>
-                    <label class="form-label" for="cod_estudiante">Código Estudiantil</label>
-                    </div>
-                </div>
-                <div class="col">
-                    @error('semestre')
-                        <span class="text-danger">{{ $message }}</span>
-                    @enderror
-                    <div class="form-outline">
-                        <select id="semestre" name="semestre" class="form-select">
-                            <option value="">Seleccione el semestre</option>
-                            @for($i = 1; $i <= 10; $i++)
-                                @if(isset($semillerista) && $semillerista->semestre == $i)
-                                    <option value="{{ $i }}" selected> Semetre{{ $i }}</option>
-                                @elseif(old('semestre') == $i)
-                                    <option value="{{ $i }}" selected> Semetre{{ $i }}</option>
-                                @else
-                                    <option value="{{ $i }}"> Semetre {{ $i }}</option>
-                                @endif
-                            @endfor
-                        </select>
-                    </div>
-                </div>
-                </div>
-                <br>
-                <div class="row">
-                    <div class="col">
-                        @error('reporte_matricula')
-                            <span class="text-danger">{{ $message }}</span>
-                        @enderror
-                        <label class="form-label" for="reporte_matricula">Cargar Reporte de Matrícula (PDF)</label>
-                        <input class="form-control form-control-lg" id="reporte_matricula" name="reporte_matricula" type="file" accept=".pdf" placeholder="Cargar Reporte de Matrícula (PDF)" />
-                    </div> 
-                </div>
-            <br>
-                <!-- Submit button -->
-                <button type="submit" class="mb-4 btn btn-success btn-block">Actualizar Información</button>
-            </form>
-        </div>
-        <br><br>
+
+
+<div class="container">
+    <center>
+    <br>
+    <ul class="list-unstyled">
+    <li class="mb-1"><i class="fas fa-check-circle me-2 text-success"></i>Datos Academicos del Semillerista @if (isset($persona)) {{$persona->nombre}} @endif</li>
+    </ul>  
+    <div class="col">
+        @if(isset($persona) && $persona->foto !== null)
+            <img class="foto-perfil" src="{{ Storage::url($persona->foto)}}" alt="Foto de Perfil">
+        @else
+            <img class="foto-perfil" src="https://distrimar.s3.amazonaws.com/static/apm/img/misc/default_user.png" alt="Imagen por Defecto">
+        @endif
     </div>
+    <br>
+    <h5><mark>Por favor asegurece de que los datos Académicos Esten Actualizados</mark></h5>
+    <br>
+    <div id="contenedor-perfil">
+        <form class="row g-3 needs-validation" novalidate action="{{ route('actualizar_datos_semillerista') }}" method="POST" enctype="multipart/form-data">
+        @csrf
+            <!-- Número del código estudiantil -->
+            <div class="col-md-6">
+                <div class="form-outline">
+                    <input type = "text" id="cod_estudiante" name="cod_estudiante" class="form-control is-valid" value="{{ isset($semillerista) ? $semillerista->cod_estudiante : old('cod_estudiante') }}"/>
+                    <label class="form-label" for="cod_estudiante">Código Estudiantil</label>
+                </div>
+                @error('cod_estudiante')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
+
+            <!-- Selección de semestre -->
+            <div class="col-md-6">
+                <select id="semestre" name="semestre" class="form-select is-valid" aria-label="Default select example">
+                    <option value="">Seleccione el semestre</option>
+                    @for($i = 1; $i <= 10; $i++)
+                        @if(isset($semillerista) && $semillerista->semestre == $i)
+                            <option value="{{ $i }}" selected> Semetre{{ $i }}</option>
+                        @elseif(old('semestre') == $i)
+                            <option value="{{ $i }}" selected> Semetre{{ $i }}</option>
+                        @else
+                            <option value="{{ $i }}"> Semetre {{ $i }}</option>
+                        @endif
+                    @endfor
+                </select>
+                @error('semestre')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
+
+            <!-- Cargar Reporte de matricula -->
+            <div class="col-md-12">
+                <label class="form-label" for="reporte_matricula" id="lb">Cargar Reporte de Matrícula (PDF)</label>
+                <div class="form-outline">
+                    <input class="form-control is-valid" id="reporte_matricula"  name="reporte_matricula" type="file" accept=".pdf" placeholder="Cargar Reporte de Matrícula (PDF)" />
+                </div>
+                @error('reporte_matricula')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
+
+            <!-- Submit button -->
+            <button type="submit" class="mb-4 btn btn-success btn-block">Actualizar Información</button>
+        </form>
+    </div>
+    </center>
+    <br><br>
+</div>
     @if (session('actualizacionExitosa'))
-      <script>
-          document.addEventListener('DOMContentLoaded', function() {
-              actualizacionExitosa();
-          });
-      </script>
+        <script>
+            document.addEventListener('DOMContentLoaded', function() {
+                actualizacionExitosa();
+            });
+        </script>
     @endif
 
     <!-- Modal -->
     <div id="reg_ext_emergente" class="modal fade" tabindex="-1" aria-labelledby="modalExitoLabel" aria-hidden="true">
-      <div class="modal-dialog">
-          <div class="modal-content">
-              <div class="modal-header">
-                  <h5 class="modal-title" id="modalExitoLabel">
-                      <h5 id="modal-titulo"></h5>
-                  </h5>
-                  <button id="cerrar-modal" type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <div class="modal-body">
-                  <div class="text-center">
-                      <i id="modal-icono"></i>
-                  </div>
-                  <p id="modalExitoMensaje" class="mt-3 text-center"></p>
-              </div>
-              <div class="modal-footer">
-                  <button widht="60%" type="button" id="btnCerrarModal" class="btn">ok</button>
-              </div>
-          </div>
-      </div>
-  </div>
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalExitoLabel">
+                        <h5 id="modal-titulo"></h5>
+                    </h5>
+                    <button id="cerrar-modal" type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="text-center">
+                        <i id="modal-icono"></i>
+                    </div>
+                    <p id="modalExitoMensaje" class="mt-3 text-center"></p>
+                </div>
+                <div class="modal-footer">
+                    <button widht="60%" type="button" id="btnCerrarModal" class="btn">ok</button>
+                </div>
+            </div>
+        </div>
+    </div>
 
 @stop
 
